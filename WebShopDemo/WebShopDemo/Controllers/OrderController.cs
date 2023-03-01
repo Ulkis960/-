1af258dfd5991eb1e39cfcee3100252cbdbc1126
ulkis960 <ulian_2004@abv.bg>
2023-03-01 12:36:31 +0200

    5.02 str

---
+++ WebShopDemo/WebShopDemo/Controllers/OrderController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using WebShopDemo.Data;
+using WebShopDemo.Models.Order;
+
+namespace WebShopDemo.Controllers
+{
+    public class OrderController : Controller
+    {
+        private readonly ApplicationDbContext context;
+
+        public OrderController(ApplicationDbContext context)
+        {
+            this.context = context;
+        }
+        public IActionResult Index()
+        {
+            string userId = this.User.FindFirstValue(ClaimTypes.NameIdentifier);
+            var user = context.Users.SingleOrDefault(u => u.Id == userId);
+
+            List<OrderIndexVM> orders = context
+                .Orders
+                .Select(x => new OrderIndexVM
+                {
+                    Id = x.Id,
+                    OrderDate = x.OrderDate.ToString("dd-MMM,yyyy hh:mm", CultureInfo.InvariantCulture),
+                    UserId = x.UserId,
+                    User = x.User.UserName,
+                    ProductId = x.ProductId,
+                    ProductName = x.Product.ProductName,
+                    Picture = x.Product.Picture,
+                    Quantity = x.Quantity,
+                    Price = x.Price,
+                    Discount = x.Discount,
+                    TotalPrice = x.TotalPrice,
+                }).ToList();
+            return View(orders);
+        }
+    }
+}
